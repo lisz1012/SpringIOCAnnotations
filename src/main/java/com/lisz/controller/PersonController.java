5514afcd853a1e760a984fc47524d6237d67e7d4
lisz1012 <lisz1012@163.com>
2020-11-29 16:55:45 -0800

    Add notes

--- src/main/java/com/lisz/controller/PersonController.java
+++ src/main/java/com/lisz/controller/PersonController.java
@@ -12,7 +12,8 @@ public class PersonController {
 
 	/**
 	 * 通过@Autowired注解能够完成自动注入的功能，默认按照类型自动注入
-	 * 如果找到一个，则直接赋值；如果没找到则抛出异常；如果找到多个就会按照变量名作为id继续装配：匹配上了继续装配，否则抛出异常
+	 * 如果找到一个，则直接赋值；如果没找到则抛出异常；如果找到多个就会按照变量名作为id,与类型首字母小写之后的结果继续查找并装配：
+	 * 匹配上了继续装配，否则抛出异常
 	 * 要存在这么个类名等于这里的变量名首字母大写，这样才可以转换为变量名对应的ID。如果变量名是personService2，但是还想要
 	 * PersonService的对象注入进来，那就要写 @Qualifier
 	 * 使用@Resource可以完成@Autowired相同的功能，但是区别是@Resource位于javax.annotation.Resource 是JDK提供的功能，
